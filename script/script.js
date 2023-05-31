491c4a60fecc0706c3b12745b2badc800eb9cbdc
nupaT <trash_jack@mail.ru>
2023-05-31 13:49:21 +0300

    add: swiper block, swiper style

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
 new Swiper('.swiper', {
+  width: 240,
+  height: 72,
+  spaceBetween: 16,
+
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
+    spaceBetween: 12,
   },
 
   speed: 400,
