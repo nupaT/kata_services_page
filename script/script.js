dec69daad30ec4dde368e7fdb5adb8bdf91a7535
nupaT <trash_jack@mail.ru>
2023-05-31 15:34:37 +0300

    add: function destroy for swiper (not work)

--- script/script.js
+++ script/script.js
@@ -1,13 +1,22 @@
-new Swiper('.swiper', {
-  width: 240,
-  height: 72,
-  spaceBetween: 16,
 
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    spaceBetween: 12,
-  },
+window.addEventListener('resize', function () {
+  if(this.innerWidth < 768) {
+    let swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      width: 240,
+      height: 72,
+      spaceBetween: 16,
+      speed: 400,
 
-  speed: 400,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        spaceBetween: 12,
+      },
+    });
+  } else {
+    swiper.destroy();
+  }
 });
+
+
